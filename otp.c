92222b04abe50a4f24315ab26dea51ed5df9973a
François "Kurikawa" Odin <francois@kurikawa.fr>
2024-11-24 00:26:58 +0100

    encipher function

--- otp.c
+++ otp.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void encipher(FILE* secret_file, FILE* output_file, FILE* key_file);
+void encipher(FILE* secret_file, FILE* output_file, FILE* key_file){
+
+    int code;
+    int key;
+    int enciphered_code;
+    // Read the file until End Of File char (EOF)
+    while( (code=fgetc(secret_file)) != EOF ){
+        key = rand(); // pick one use key with random
+        enciphered_code = code ^ key; // code XOR key
+        // write output files
+        fputc(key, key_file);
+        fputc(enciphered_code, output_file);
+    }
+
+}
 
 void decipher(FILE* secret_file, FILE* output_file, FILE* key_file);
 
