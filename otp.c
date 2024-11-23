ae70aa76fa504a2c0a1a9336e27c04a099099162
François "Kurikawa" Odin <francois@kurikawa.fr>
2024-11-24 00:22:28 +0100

    encipher & decipher signatures

--- otp.c
+++ otp.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void encipher(FILE* secret_file, FILE* output_file, FILE* key_file);
+
+void decipher(FILE* secret_file, FILE* output_file, FILE* key_file);
+
 int main(int argc, char* argv[]){
     return EXIT_SUCCESS;
 }
